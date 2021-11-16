7fd1cb61d2277e00665dd0646e9638c664d12b0e
mkmau <mkmau@hawaii.edu>
2021-11-16 11:47:24 -1000

    Assignment1 Work

---
+++ mau_marcus_assignment1/public/product.js
@@ -0,0 +1,34 @@
+// Marcus' Product Array for Assignment 1
+products = [
+{
+    "name": "Something",
+    "price": 1.99,
+    "description": "This is something good",
+    "image" : "./images/Valley1.jpg"
+},
+
+{
+    "name": "Something",
+    "price": 1.99,
+    "description": "This is something good",
+    "image" : "./images/Valley2.jpg"
+},
+
+{
+    "name": "Something",
+    "price": 1.99,
+    "description": "This is something good",
+    "image" : "./images/Valley1.jpg"
+},
+
+{
+    "name": "Something",
+    "price": 1.99,
+    "description": "This is something good",
+    "image" : "./images/Valley2.jpg"
+},
+]
+
+if (typeof module != 'undefined') {
+    module.exports.products = products;
+}
