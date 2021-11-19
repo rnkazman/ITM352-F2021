a94cabc4917cf9c42ad5d8424fed4d39b4afcf57
mkmau <mkmau@hawaii.edu>
2021-11-18 23:47:02 -1000

    Assignment1 Work

---
+++ TRIAL1NOGREEn/public/products.js
@@ -0,0 +1,49 @@
+// Marcus' Product Array for Assignment 1
+products = [
+{
+    "name": "Asia CD",
+    "price": 1.99,
+    "image" : "./images/Asia.jpg",
+    "quantity_available" : 14,
+},
+
+{
+    "name": "Cecelio and Kapono CD",
+    "price": 1.99,
+    "image" : "./images/CandK.jpg",
+    "quantity_available" : 10,
+},
+
+{
+    "name": "Hall and Oates CD",
+    "price": 1.99,
+    "image" : "./images/HallandOates.jpg",
+    "quantity_available" : 18,
+},
+
+{
+    "name": "Outfield CD",
+    "price": 1.99,
+    "image" : "./images/Outfield.jpg",
+    "quantity_available" : 19,
+},
+
+{
+    "name": "Toto CD",
+    "price": 1.99,
+    "image" : "./images/Toto.jpg",
+    "quantity_available" : 15,
+},
+
+{
+    "name": "Tears for Fears CD",
+    "price": 1.99,
+    "image" : "./images/Tears.jpg",
+    "quantity_available" : 13,
+},
+]
+
+
+if (typeof module != 'undefined') {
+    module.exports.products = products;
+}
