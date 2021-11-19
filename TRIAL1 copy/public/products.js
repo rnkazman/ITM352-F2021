e8b4e4a67094c04f38b0c211fecc06838cfaf6aa
mkmau <mkmau@hawaii.edu>
2021-11-18 21:38:01 -1000

    A1 Work

---
+++ TRIAL1 copy/public/products.js
@@ -0,0 +1,41 @@
+// Marcus' Product Array for Assignment 1
+products = [
+{
+    "name": "Something",
+    "price": 1.99,
+    "image" : "./images/Valley1.jpg",
+    "quantity_available" : 3,
+},
+
+{
+    "name": "Something",
+    "price": 1.99,
+    "image" : "./images/Valley2.jpg",
+    "quantity_available" : 3,
+},
+
+{
+    "name": "Something",
+    "price": 1.99,
+    "image" : "./images/Valley1.jpg",
+    "quantity_available" : 3,
+},
+
+{
+    "name": "Something",
+    "price": 1.99,
+    "image" : "./images/Valley2.jpg",
+    "quantity_available" : 3,
+},
+
+{
+    "name": "Something",
+    "price": 1.99,
+    "image" : "./images/Valley2.jpg",
+    "quantity_available" : 3,
+},
+]
+
+if (typeof module != 'undefined') {
+    module.exports.products = products;
+}
