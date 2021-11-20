324a4773157a7f9ba0ed69a8ecb0391c55578c84
mkmau <mkmau@hawaii.edu>
2021-11-19 23:17:58 -1000

    A1 WOrk

--- SideFiles/Mau_Marcus_Assignment1/server.js
+++ SideFiles/Mau_Marcus_Assignment1/server.js
@@ -26,26 +26,30 @@ app.use(express.urlencoded({ extended: true })); //get data in the body
     let POST = request.body; // data would be packaged in the body
 
     //taken from Alyssa Mencel
-    //ATTEMPT at taking quantity_available values from products.js and making a for loop to subtract quantity ordered from available
     //check if quantities are nonnegative integers 
-    if (typeof POST['submitPurchase'] != 'undefined') { 
+    if (typeof POST['submitPurchase'] != 'undefined') {
         var hasvalidquantities=true; // creating a varibale assuming that it'll be true
-        var hasquantities=false 
+        var hasquantities=false
         for (i = 0; i < products.length; i++) {
-            let quantityremaining = products_array[i].quantity_available - products_array[i].total_sold;
-                qty=POST[`quantity${i}`];
-                hasquantities=hasquantities || qty>0; // If it has a value bigger than 0 then it is good
-                hasvalidquantities=hasvalidquantities && isNonNegativeInteger(qty);    // if it is both a quantity over 0 and is valid    
-} 
+            
+                        qty=POST[`quantity${i}`];
+                        hasquantities=hasquantities || qty>0; // If it has a value bigger than 0 then it is good
+                        hasvalidquantities=hasvalidquantities && isNonNegativeInteger(qty);    // if it is both a quantity over 0 and is valid    
+        } 
 
         //copied from Alyssa Mencel
-        // if all quantities are valid, generate the invoice
-        //redirect if correct values inputted to the invoice page
-        //redirect if the no correct value to the products_display page
+        for (i = 0; i < products.length; i++) {
+            let quantityremaining = products_array[i].quantity_available - products_array[i].total_sold;
+            qty=POST[`quantity${i}`];
+            hasquantities=hasquantities || qty>0; // If it has a value bigger than 0 then it is good
+            hasvalidquantities=hasvalidquantities && isNonNegativeInteger(qty);    // if it is both a quantity over 0 and is valid    
+} 
+        //copied from Alyssa Mencel
+        // if all quantities are valid, generate the invoice// 
         const stringified = queryString.stringify(POST);
         if (hasvalidquantities && hasquantities) {
       
-            response.redirect("./invoice.html?" + stringified); // using the invoice.html and all the data that is input
+            response.redirect("./invoice.html?"+stringified); // using the invoice.html and all the data that is input
         }  
 
        
@@ -55,10 +59,7 @@ app.use(express.urlencoded({ extended: true })); //get data in the body
     }
 });
 
-
 //repeats the isNonNegInt function from the products_display.html file 
-
-
 function isNonNegativeInteger(q, returnErrors = false) {
     errors = []; // assume that quantity data is valid 
     if (q == "") { q = 0; }
@@ -68,9 +69,7 @@ function isNonNegativeInteger(q, returnErrors = false) {
     return returnErrors ? errors : (errors.length == 0);
 }
 
-
 // taken from assignment 1 examples
 app.use(express.static('./public')); // root in the 'public' directory so that express will serve up files from here
 app.listen(8080, () => console.log(`listening on port 8080`)); //run the server on port 8080 and show it in the console
 
-           
