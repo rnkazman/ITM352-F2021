cc540f19c47043e8700e1f8abde8983d1b8bdcdd
mkmau <mkmau@hawaii.edu>
2022-03-02 21:00:39 +0100

    Assignment work

---
+++ WA2 test/Notes.js
@@ -0,0 +1,9 @@
+require("./whatever products page is being used");
+
+var num_products = 5; // 
+
+var count = 1; 
+while(count <= num_products) {
+    console.log(`${count}. ${eval('name' + count)}`); //pulls information from the products_data.js page and can put it on the server page
+    count++;
+}
