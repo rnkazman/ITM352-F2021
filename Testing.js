89070cfdbf58e833b913ea98413ee906ceef41aa
mkmau <mkmau@hawaii.edu>
2022-03-04 17:13:59 +0100

    Notes

--- Testing.js
+++ Testing.js
@@ -1,4 +1,10 @@
-groceries = ["eggs", "milk"]
+/* deposit = function(amount) { // can also do function deposit(amount)
+    console.log(balance);
+    balance += amount; 
+    console.log("New balance is " + balance);
+}
 
-console.log (groceries)
-    
+var balance = 600
+deposit (50); // tried to do var deposit = 50; but that did not work - is it because deposit is already assigned to function?
+console.log("Balance is " + balance);
+*/ 
