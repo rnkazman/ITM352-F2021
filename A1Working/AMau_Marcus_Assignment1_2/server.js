cde155e2198b20afaf6d49aae936efaeced634d5
mkmau <mkmau@hawaii.edu>
2022-03-08 12:02:26 +0100

    Assignment1 Work

--- A1Working/AMau_Marcus_Assignment1_2/server.js
+++ A1Working/AMau_Marcus_Assignment1_2/server.js
@@ -32,11 +32,12 @@ app.use(express.urlencoded({ extended: true })); //get data in the body
         var hasvalidquantities=true; // this is to make sure quantity is valid
         var hasquantities=false; 
         for (i = 0; i < products.length; i++) { //for loop to go through the products and check which one was bought
-            var quantityremaining = products_array[i].quantity_available - products_array[i].total_sold; // Deduct quantity from inventory
+            var quantityremaining = products_array[i].quantity_available - products_array[i].quantity_available; // Deduct quantity from inventory
                 qty=POST[`quantity${i}`];
                 hasquantities=hasquantities || qty>0; // If it has a value bigger than 0 then it is good
                 hasvalidquantities=hasvalidquantities && isNonNegInt(qty);    // if it is both a quantity over 0 and is valid    
 } 
+console.log(quantityremaining)
 
         //copied from Alyssa Mencel
         // if all quantities are valid, generate the invoice
